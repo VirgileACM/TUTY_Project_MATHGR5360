486ca5a26892d05a48db2ba161d09bbb11ee7f49
KJonesUCF <kevin.jones2694@yahoo.com>
2017-04-15 13:57:21 -0400

    Update calculateTrades.m
    I updated the trading code to accurately reflect what the professor described in class. The last code identified a signal and then traded in the NEXT bar. Now the code identifies a signal and trades at the current bar either at the breakout point (the running max or min) or at the open (if there was a gap in the price).
    
    I also updated the stpPct part of the trading strategy. Before I was only updating the peak/trough based on each bar's open, but now it updates based on the high/low. And like above, I took care of the possibility of a gap.

--- channel_trading_MatlabCode/calculateTrades.m
+++ channel_trading_MatlabCode/calculateTrades.m
@@ -4,17 +4,18 @@
 %Code to generate our trading signals.
 %Will use our boolean signals based on breakouts and stop percentage.
 
-function [trade] = calculateTrades(t, stpPct, signal, open, close)
+function [trade, price] = calculateTrades(t, stpPct, signal, runningMax, runningMin, open, high, low, close)
 
-%Initialize our trade vector.
+%Initialize our trade and price vector.
 n = size(open, 1);
 trade = zeros(n, 1);
+price = zeros(n,1);
 
 currentPos = 0;
 prevPeak = 0.00;
 prevTrough = 0.00;
 
-for i = (t+2):(n-1)
+for i = (t+1):(n-1)
     
     %If we currently are flat, we need to simply look at our breakout
     %signals we generated in calculateSignal.
@@ -23,18 +24,22 @@ for i = (t+2):(n-1)
         %If the signal for the previous bar is 1, that is telling us there is a breakout to the
         %upside and to go long the future. Need to initialize prevPeak to
         %where we bought the futures contract at.
-        if signal(i-1) == 1
+        if signal(i) == 1
             trade(i) = 1;
             currentPos = 1;
-            prevPeak = open(i);
+            %We use the max function in case there was a gap in the price.
+            price(i) = max(runningMax(i-1), open(i));
+            prevPeak = price(i);
             
         %If the signal for the previous bar is -1, that is telling us there is a breakout to the
         %downside and to go short the future. Need to initialize prevTrough
         %to where we sold the futures ocntract at
-        elseif signal(i-1) == -1
+        elseif signal(i) == -1
             trade(i) = -1;
             currentPos = -1;
-            prevTrough = open(i);
+            %We use the min function in case there was a gap in the price.
+            price(i) = min(runningMin(i-1),open(i));
+            prevTrough = price(i);
         end
         
     %If we are currently long the futures contract, we now don't need to
@@ -42,10 +47,12 @@ for i = (t+2):(n-1)
     %and exit the position when we hit our stop loss.   
     elseif currentPos == 1
         
-        if close(i-1) > prevPeak
-            prevPeak = close(i-1);
-        elseif close(i-1) < (1-stpPct) * prevPeak
+        if high(i-1) > prevPeak
+            prevPeak = high(i-1);
+        elseif low(i) < (1-stpPct) * prevPeak
             trade(i) = -1;
+            %We use the min function in case there was a gap in the price.
+            price(i) = min(open(i), (1-stpPct)*prevPeak);
             currentPos = 0;
         end
     
@@ -53,10 +60,12 @@ for i = (t+2):(n-1)
     %look at the signal. We simply keep updating prevTrough (if necessary)
     %and exit the position when we hit our stop loss.   
     elseif currentPos == -1
-        if close(i-1) < prevTrough
-            prevTrough = close(i-1);
-        elseif close(i-1) > (1+stpPct) * prevTrough
+        if low(i-1) < prevTrough
+            prevTrough = low(i-1);
+        elseif high(i-1) > (1+stpPct) * prevTrough
             trade(i) = 1;
+            %We use the max function in case there was a gap in the price.
+            price(i) = max(open(i), (1+stpPct)*prevTrough);
             currentPos = 0;
         end
     end
@@ -65,9 +74,8 @@ end
 %Make sure we close out any position we have at the end of the sample.
 if currentPos == 1
     trade(n) = -1;
+    price(n) = close(n);
 elseif currentPos == -1
     trade(n) = 1;
+    price(n) = close(n);
 end
-        
-        
-        
